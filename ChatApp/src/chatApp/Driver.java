f95d3376157e418920e19945ed57d46097d16d17
TinTin Kalaw <tintin.kalaw@gmail.com>
2015-11-03 12:33:07 +0800

    Initial upload.
    - Can broadcast message to all clients.
    - Source link is in the Driver Class
    - TO-DO: add code to handle private messaging

---
+++ ChatApp/src/chatApp/Driver.java
@@ -0,0 +1,28 @@
+package chatApp;
+
+/**
+ * SOURCE: http://cs.lmu.edu/~ray/notes/javanetexamples/#chat
+ * @author addicteduser
+ */
+public class Driver {
+	private final static String host = "localhost";
+	private final static int port = 8888;
+	
+	//public static void main(String[] args) {
+
+
+	//}
+	/**
+	 * @return the host
+	 */
+	public static String getHost() {
+		return host;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public static int getPort() {
+		return port;
+	}
+}
