092d14e49aaabf507128471f2b05cbabb44f83ec
TinTin Kalaw <tintin.kalaw@gmail.com>
2015-11-03 13:40:58 +0800

    Cleaned server code

--- ChatApp/src/chatApp/Driver.java
+++ ChatApp/src/chatApp/Driver.java
@@ -1,5 +1,9 @@
 package chatApp;
 
+import java.io.IOException;
+
+import server.ChatServer;
+
 /**
  * SOURCE: http://cs.lmu.edu/~ray/notes/javanetexamples/#chat
  * @author addicteduser
@@ -8,10 +12,16 @@ public class Driver {
 	private final static String host = "localhost";
 	private final static int port = 8888;
 	
-	//public static void main(String[] args) {
+	public static void main(String[] args) {
+		try {
+			ChatServer server = new ChatServer();
+			server.run();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-
-	//}
+	}
 	/**
 	 * @return the host
 	 */
--- ChatApp/src/server/ChatServer.java
+++ ChatApp/src/server/ChatServer.java
@@ -11,23 +11,15 @@ import java.util.HashSet;
 import chatApp.Driver;
 
 /**
- * A multithreaded chat room server.  When a client connects the
- * server requests a screen name by sending the client the
- * text "SUBMITNAME", and keeps requesting a name until
- * a unique one is received.  After a client submits a unique
- * name, the server acknowledges with "NAMEACCEPTED".  Then
- * all messages from that client will be broadcast to all other
- * clients that have submitted a unique screen name.  The
- * broadcast messages are prefixed with "MESSAGE ".
- *
- * Because this is just a teaching example to illustrate a simple
- * chat server, there are a few features that have been left out.
- * Two are very useful and belong in production code:
- *
- *     1. The protocol should be enhanced so that the client can
- *        send clean disconnect messages to the server.
- *
- *     2. The server should do some logging.
+ * A multithreaded chat room server. 
+ * When a client connects, the server requests a
+ * screen name by sending the client the text "SUBMITNAME",
+ * and keeps requesting a name until a unique one is received.
+ * After a client submits a unique name, the server
+ * acknowledges with "NAMEACCEPTED". Then all messages from
+ * that client will be broadcast to all other clients that
+ * have submitted a unique screen name. The broadcast messages
+ * are prefixed with "MESSAGE".
  */
 public class ChatServer extends Thread {
 	/**
@@ -38,104 +30,146 @@ public class ChatServer extends Thread {
 	 * Set of all PrintWriter for each client.
 	 */
 	private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
-	
-	public static void main(String[] args) throws Exception {
-        System.out.println("[SERVER] The chat server is running.");
-        ServerSocket listener = new ServerSocket(Driver.getPort());
-        try {
-        	// Listen for client connections
-            while (true) {
-                new Handler(listener.accept()).start();
-            }
-        } finally {
-            listener.close();
-        }
-    }
-	
+
+	public ChatServer() throws IOException {
+		System.out.println("[SERVER] The chat server is running.");
+		ServerSocket listener = new ServerSocket(Driver.getPort());
+
+		try {
+			// Listen for client connections
+			while (true) {
+				new ClientManager(listener.accept()).start();
+			}
+		} finally {
+			listener.close();
+		}
+	}
+
 	/**
-     * A handler thread class.  Handlers are spawned from the listening
-     * loop and are responsible for a dealing with a single client
-     * and broadcasting its messages.
-     */
-    private static class Handler extends Thread {
-        private String name;
-        private Socket connection;
-        private BufferedReader in;
-        private PrintWriter out;
+	 * A client manager thread class. 
+	 * Client managers are spawned from the listening loop of the server
+	 * and are responsible for a dealing with a single client and broadcasting its messages.
+	 */
+	private static class ClientManager extends Thread {
+		private String name;
+		private Socket connection;
+		private BufferedReader in;
+		private PrintWriter out;
 
-        /**
-         * Constructs a handler thread, squirreling away the socket.
-         * All the interesting work is done in the run method.
-         */
-        public Handler(Socket socket) {
-            this.connection = socket;
-        }
+		/**
+		 * Constructs a client manager thread, squirreling away the socket.
+		 */
+		public ClientManager(Socket socket) {
+			System.out.println("[SERVER] New client connection.");
+			this.connection = socket;
+		}
 
-        /**
-         * Services this thread's client by repeatedly requesting a
-         * screen name until a unique one has been submitted, then
-         * acknowledges the name and registers the output stream for
-         * the client in a global set, then repeatedly gets inputs and
-         * broadcasts them.
-         */
-        public void run() {
-            try {
+		/**
+		 * Services this thread's client by repeatedly requesting a
+		 * screen name until a unique one has been submitted, then
+		 * acknowledges the name and registers the output stream for
+		 * the client in a global set, then repeatedly gets inputs and
+		 * broadcasts them.
+		 */
+		public void run() {
+			try {
+				initializeIOStreams();
+				requestClientName();
+				acceptClient();
+				sendMessages();
+				// TODO: put code handling private messaging in SEND MESSAGE				
+			} catch (IOException e) {
+				System.err.println("[SERVER] ERROR: " + e);
+			} finally {
+				closeClient();
+			}
+		}
 
-                // Create character streams for the socket.
-                in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                out = new PrintWriter(connection.getOutputStream(), true);
+		/**
+		 * Create character streams for the socket.
+		 */
+		private void initializeIOStreams() throws IOException {
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			out = new PrintWriter(connection.getOutputStream(), true);
+		}
 
-                // Request a name from this client.  Keep requesting until
-                // a name is submitted that is not already used.  Note that
-                // checking for the existence of a name and adding the name
-                // must be done while locking the set of names.
-                while (true) {
-                    out.println("SUBMITNAME");
-                    name = in.readLine();
-                    if (name == null) {
-                        return;
-                    }
-                    synchronized (names) {
-                        if (!names.contains(name)) {
-                            names.add(name);
-                            break;
-                        }
-                    }
-                }
+		/**
+		 * Request a name from this client.  Keep requesting until
+		 * a name is submitted that is not already used.  Note that
+		 * checking for the existence of a name and adding the name
+		 * must be done while locking the set of names.
+		 * @throws IOException
+		 */
+		private void requestClientName() throws IOException {
+			boolean isNameTaken = true;
+			
+			do {
+				out.println("SUBMITNAME");
+				name = in.readLine();
 
-                // Now that a successful name has been chosen, add the
-                // socket's print writer to the set of all writers so
-                // this client can receive broadcast messages.
-                out.println("NAMEACCEPTED");
-                writers.add(out);
+				// empty string
+				if (name == null) {
+					isNameTaken = true;
+				}
+				
+				// check if name is taken
+				synchronized (names) {
+					if (!names.contains(name)) {
+						names.add(name);
+						isNameTaken = false;
+					} else
+						isNameTaken = true;
+				}
+			} while(isNameTaken);
+		}
+		
+		/**
+		 * Now that a successful name has been chosen, add the
+		 * socket's print writer to the set of all writers so
+		 * this client can receive broadcast messages.
+		 */
+		private void acceptClient() {
+			out.println("NAMEACCEPTED");
+			writers.add(out);
+		}
 
-                // Accept messages from this client and broadcast them.
-                // Ignore other clients that cannot be broadcasted to.
-                while (true) {
-                    String input = in.readLine();
-                    if (input == null) {
-                        return;
-                    }
-                    for (PrintWriter writer : writers) {
-                        writer.println("MESSAGE " + name + ": " + input);
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println(e);
-            } finally {
-                // This client is going down!  Remove its name and its print
-                // writer from the sets, and close its socket.
-                if (name != null) {
-                    names.remove(name);
-                }
-                if (out != null) {
-                    writers.remove(out);
-                }
-                try {
-                    connection.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-    }
+		/**
+		 *  Accept messages from this client and broadcast them.
+		 *  Ignore other clients that cannot be broadcasted to.
+		 * @throws IOException
+		 */
+		private void sendMessages() throws IOException {
+			while (true) {
+				String input = in.readLine();
+				
+				// if empty
+				if (input == null) {
+					return;
+				}
+				
+				// broadcast message
+				for (PrintWriter writer : writers) {
+					writer.println("MESSAGE " + name + ": " + input);
+				}
+			}
+		}
+	
+		/**
+		 * This client is going down!  Remove its name and its print
+		 * writer from the sets, and close its socket.
+		 */
+		private void closeClient() { 
+			if (name != null) {
+				names.remove(name);
+			}
+			
+			if (out != null) {
+				writers.remove(out);
+			}
+			
+			try {
+				connection.close();
+			} catch (IOException e) { }
+		}
+	}
 }
